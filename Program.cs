33bd8ecf3d72751ca3b0e996e53c4866ef234158
Silas Eduardo <silas@MALFESCRNB160.automacao.ipatinga>
2022-09-04 18:33:25 -0300

    retirando parte do código antigo não usados

--- Program.cs
+++ Program.cs
@@ -17,11 +17,11 @@ public class Program
     static int linhaExcel; //Indica qual linha da planilha receberá os dados
     static string nomefoto = ""; //copia o nome de cada foto
 
-    static string pastadefotos = "C:\\Users\\silas\\Desktop\\digitainova\\"; // caminho da pasta com as fotos tiradas das caixas 
+    static string pastadefotos = "C:\\Users\\silas\\Desktop\\digitainova\\"; // caminho da pasta com as fotos originais tiradas das caixas 
 
     static string fotosdigitalizadas = "C:\\Users\\silas\\Desktop\\digitalizadas\\"; // caminho da pasta onde as fotos pós digitalizadas serão armazenadas
 
-    static string pathExcel = @"C:\Users\silas\Desktop\digitalizadas\EGL.xlsx";
+    static string pathExcel = @"C:\Users\silas\Desktop\digitalizadas\EGL.xlsx"; // Caminho da Planilha Excel que será gerada 
     public static void Inicio()
     {
 
@@ -168,113 +168,6 @@ public class Program
             {
                 pixelColor = myBitmap.GetPixel(coluna, linha);
 
-                // Console.WriteLine(pixelColor.GetBrightness()+"  "+pixelColor.GetSaturation());
-                // cores.Add(pixelColor.GetHue());
-
-                // var hsvcolor = new Hsv { H = pixelColor.GetHue(), S = pixelColor.GetSaturation(), V = pixelColor.GetBrightness() };
-
-                // var rgbconvert = rgbcolor.To<Hsv>();
-                // if ((hsvcolor.H >330 ) || (hsvcolor.H > -330 ) )
-                // {
-                // hsvcolor.H = 0;
-                // }
-                // if (hsvcolor.S < 0)
-                // {
-                //     hsvcolor.S = hsvcolor.S * -1;
-                // }
-                // if (hsvcolor.V < 0)
-                // {
-                //     hsvcolor.V = hsvcolor.V * -1;
-                // }
-
-                // var hsvcolor2 = new Hsv { H = 100, S = 0.9, V = hsvcolor.V };
-
-                // // var newrgb = new Rgb{ R = pixelColor.R, G = 100, B=pixelColor.B };
-                // var newrgb = hsvcolor2.To<Rgb>();
-
-                // Console.WriteLine((newrgb.R)+"|"+ (newrgb.G)+" | " +(newrgb.B));
-
-
-                // while (newrgb.R > 255 || newrgb.R < 0 ||
-                // newrgb.G > 255 || newrgb.G < 0 ||
-                // newrgb.B > 255 || newrgb.B < 0)
-                // {
-                //     if ((int)newrgb.R > 255)
-                //     {
-                //         newrgb.R = 255;
-                //     }
-                //     else if ((int)newrgb.R < 0)
-                //     {
-                //         newrgb.R = 0;
-                //     }
-                //     if ((int)newrgb.G > 255)
-                //     {
-                //         newrgb.G = 255;
-                //     }
-                //     else if ((int)newrgb.G < 0)
-                //     {
-                //         newrgb.G = 0;
-                //     }
-                //     if ((int)newrgb.B > 255)
-                //     {
-                //         newrgb.B = 255;
-                //     }
-                //     else if ((int)newrgb.B < 0)
-                //     {
-                //         newrgb.B = 0;
-                //     }
-                // }
-
-                // if (newrgb.R > 255 || newrgb.R < 0 ||
-                // newrgb.G > 255 || newrgb.G < 0 ||
-                //  newrgb.B > 255 || newrgb.B < 0)
-                // {
-                // }
-                // else
-                // {
-                // myBitmap.SetPixel(coluna, linha, Color.FromArgb((int)newrgb.R, (int)newrgb.G, (int)newrgb.B));
-
-                // }
-                //     if (newrgb.R < 145 && newrgb.G < 145 && newrgb.B < 145)
-                //     {
-                //         newrgb.R = 0;
-                //         newrgb.G = 0;
-                //         newrgb.B = 0;
-                //     }
-                //     else if ((newrgb.R - newrgb.G) > 12 ||
-                //    (newrgb.R - newrgb.G) < -12 ||
-                //    (newrgb.R - newrgb.B) > 12 ||
-                //    (newrgb.R - newrgb.B) < -12 ||
-                //    (newrgb.G - newrgb.B) > 12 ||
-                //    (newrgb.G - newrgb.B) < -12)
-                //     {
-                //         newrgb.R = 0;
-                //         newrgb.G = 0;
-                //         newrgb.B = 0;
-                //     }
-
-                // myBitmap.SetPixel(coluna, linha, Color.FromArgb((int)newrgb.R,(int)newrgb.G,(int)newrgb.B));
-                // if ((newrgb.R >= 0 && newrgb.R <= 255) && (newrgb.G >= 0 && newrgb.G <= 255) && (newrgb.B >= 0 && newrgb.B <= 255))
-                // {
-                //     // Console.WriteLine(newrgb.R+" "+ newrgb.G+ " "+ newrgb.B);
-                //     myBitmap.SetPixel(coluna, linha, Color.FromArgb((int)newrgb.R, (int)newrgb.G, (int)newrgb.B));
-
-                // }
-                // Console.WriteLine(hsvcolor);
-
-
-                // if (newrgb.R != 255 && newrgb.G != 255 && newrgb.B != 255)
-                // {
-                //    Console.WriteLine("Coluna: " + coluna + " | Linha: " + linha);
-                // }
-
-
-                // if ((newrgb.R >= 105 && newrgb.R <= 192) && (newrgb.G >= 105 && newrgb.G <= 192) && (newrgb.B >= 105 && newrgb.B <= 192))
-                // {
-                //     //myBitmap.SetPixel(coluna, linha, Color.Red);
-                //     array[linha, coluna] = 100;
-
-                //}
                 if ((pixelColor.G >= 140))
                 {
                     int diff = pixelColor.G - pixelColor.B;
@@ -338,30 +231,12 @@ public class Program
         var sheet = wb.GetSheet("0101");
 
 
-        // Console.WriteLine("Salvar Excel iniciado");
-
-        //excelSheet.Cells[1, 1] = 1;
-
         int linha;
         int coluna;
 
         long soma = 0;
 
 
-        //for (linha = 0; linha < linhaMax; linha++)
-        //{
-        //    for (coluna = 0; coluna < colunaMax; coluna++)
-        //    {
-        //        //excelSheet.Cells[linha + 1, coluna + 1] = array[linha, coluna];
-
-        //        soma = array[linha, coluna] + soma;
-
-        //    }
-
-        //    excelSheet.Cells[1, linha + 1] = soma / colunaMax;
-        //    soma = 0;
-
-        //}
 
         float media;
 
@@ -374,9 +249,7 @@ public class Program
 
 
             media = (float)soma / (float)linhaMax;
-            // Console.WriteLine($"Media coluna {coluna}: {media}");
-            //excelSheet.Cells[1, coluna + 1] = media; //Antigo, só gravava na linha 1
-
+  
 
 
             var row = sheet.GetRow(linhaExcel) ?? sheet.CreateRow(linhaExcel);
